052d1960fdd37318eb11fda0307dc649fdefc341
Harsha <harshavardhanraju1997@gmail.com>
2025-12-09 20:56:46 +0530

    added console log

--- middleware/logger.js
+++ middleware/logger.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 exports.loggerMiddleware = function (req, res, next) {
   {
     const log = `\n${new Date().toLocaleString()}  : Method is ${req.method} : Path is  ${req.path}`;
-    console.log(log);
+    console.log("Log is:::"+log);
 
     fs.appendFileSync("log.txt", log, "utf-8");
     next();
